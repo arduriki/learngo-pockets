f303792335484c2ebead9ee444ff2532d7e53a9f
Jordi Ardura <jordia@btic.cat>
2025-07-01 09:40:34 +0000

    hello cli app done

---
+++ hello/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var lang string
+	// memory addr, name of the option, default value, short description of the flag.
+	flag.StringVar(&lang, "lang", "en", "The required language, e.g. en, ur...")
+	// to scan the input parameter and fills every variable we've identified as a receiver.
+	flag.Parse()
+
+	greeting := greet(language(lang))
+	fmt.Println(greeting)
+}
+
+// language represents the language's code
+type language string
+
+// phrasebook holds greeting for each supported language
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+// greet says hello to the world in various languages
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
+}

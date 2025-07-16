d01cc586a7be7d15721bfbc4eb3f42740bdf851e
Jordi Ardura <jordi.ardura.professional@gmail.com>
2025-07-16 15:43:28 +0000

    hello world extended app

--- hello/main.go
+++ hello/main.go
@@ -1,23 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	greeting := greet("en")
+	// hold the value provided by the user
+	var lang string
+	flag.StringVar(&lang, "lang", "en", "The required language, e.g. en, ur...")
+	// to scan the input parameters
+	flag.Parse()
+	// test it with: go run main.go -lang=el
+
+	greeting := greet(language(lang))
 	fmt.Println(greeting)
 }
 
 // language represents the language's code
 type language string
 
+// phrasebook holds greeting for each supported language
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+	"ca": "Hola món!",         // Catalan
+}
+
 // greet says hello to the world in the specified language
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello world"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		return ""
+	// ok -> if exists...
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+
+	return greeting
 }
